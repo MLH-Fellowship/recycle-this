4774b16489f961e9a14bc658ac4f384f15064353
Diana Mukhanova <diananova2508@gmail.com>
2020-10-27 22:36:57 +0100

    added leaderboard library

--- src/screens/GameOver.js
+++ src/screens/GameOver.js
@@ -1,12 +1,16 @@
 
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Button, FlatList } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import Leaderboard from 'react-native-leaderboard';
+import Constants from './../Constants';
+const WIDTH = Constants.WIDTH;
+const HEIGHT = Constants.HEIGHT;
 
 
 const GameOver = ({route, navigation}) => {
   const [points, setPoints] = useState([{}]);
+  let iconURL = 'https://www.flaticon.com/svg/static/icons/svg/860/860784.svg'
 
   
   const getData = async () => {
@@ -23,6 +27,44 @@ const GameOver = ({route, navigation}) => {
    getData();
   });
 
+  /*
+  const renderItem = ({ entry, index }) => (
+    <View>
+    <Text>{index+1}</Text> 
+    <Text>poooints {JSON.stringify(entry)}</Text>
+    </View>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={points}
+        renderItem={(item, index) => renderItem(item, index)}
+      />
+    </SafeAreaView>
+  );
+}*/
+
+
+
+return(
+  <View style={styles.container}>
+    <View>
+    <Text style={styles.title}>Leaderboard</Text>
+    <Text>You got </Text>
+    </View>
+  <Leaderboard 
+  data={points} 
+  sortBy='points' 
+  labelBy='username'
+  icon={iconURL}
+  oddRowColor='lightyelllow'
+  evenRowColor='peachpuff'
+  containerStyle={styles.leaderboard}
+  />
+  </View>
+)}
+/*
 <Text>Leaderboard {JSON.stringify(points)}</Text>
     return(
     <View style={styles.scoreboard}>
@@ -48,12 +90,21 @@ const GameOver = ({route, navigation}) => {
       />
     </View>
     );
-  };
+  };*/
 
   
 const styles = StyleSheet.create({
-  scoreboard: {
-    backgroundColor: '#B8E994',
+  container: {
+    flex:1
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 20
+  },
+  leaderboard: {
+    flex: 0.5,
+    height: HEIGHT*0.5,
+    backgroundColor: 'red',
   },
 });
    
